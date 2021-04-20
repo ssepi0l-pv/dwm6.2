3b14a87354ac7de9ceb1c802946eeaf5ebac7370
Shmuel Pksvan <yzombies16@gmail.com>
2021-04-20 19:28:52 -0400

    Added old dwm build into old branch

---
+++ olddwm/config.mk
@@ -0,0 +1,38 @@
+# dwm version
+VERSION = 5.8
+
+# Customize below to fit your system
+
+# paths
+PREFIX = /usr/local
+MANPREFIX = ${PREFIX}/share/man
+
+X11INC = /usr/include/X11
+X11LIB = /usr/lib/X11
+
+# Xinerama
+XINERAMALIBS = -L${X11LIB} -lXinerama
+XINERAMAFLAGS = -DXINERAMA
+
+# includes and libs
+INCS = -I. -I/usr/include -I${X11INC}
+#LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 ${XINERAMALIBS}
+#LIBS = -L/usr/lib -lX11 ${XINERAMALIBS} ${FREETYPELIBS} -lXrender
+LIBS = -L/usr/lib -lX11 ${XINERAMALIBS} ${FREETYPELIBS} -lX11-xcb -lxcb -lxcb-res ${KVMLIB}
+
+
+# flags
+CPPFLAGS = -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
+#CFLAGS = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
+CFLAGS = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
+#LDFLAGS = -g ${LIBS}
+LDFLAGS = -s ${LIBS}
+
+# Solaris
+#CFLAGS = -fast ${INCS} -DVERSION=\"${VERSION}\"
+#LDFLAGS = ${LIBS}
+#KVMLIB = -lkvm
+
+
+# compiler and linker
+CC = cc
