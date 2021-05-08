0e90782fcf65fb16d0bd8c2e7b09195be9c63e47
Shmuel Pksvan <yzombies16@gmail.com>
2021-05-08 18:08:17 -0400

    Added branch 'testing'. dwm-rainbowtags was added succesfully to 'testing' build.

--- config.def.h
+++ config.def.h
@@ -39,6 +39,30 @@ static const unsigned int alphas[][3]      = {
 static const char *tags[] = { "", "", "ﭮ", "", "嗢", "", "", "", "" };
 static const char *alttags[] = { "> ", "> ", "> ﭮ", "> ", "> 嗢", "> ", "> ", "> ", "> " };
 
+static const char *tagsel[][2] = {
+	{ "#ffffff", "#ff0000" },
+	{ "#ffffff", "#ff7f00" },
+	{ "#000000", "#ffff00" },
+	{ "#000000", "#00ff00" },
+	{ "#ffffff", "#0000ff" },
+	{ "#ffffff", "#4b0082" },
+	{ "#ffffff", "#9400d3" },
+	{ "#000000", "#ffffff" },
+	{ "#ffffff", "#000000" },
+};
+
+static const char *tagsel[][2] = {
+	{ "#ffffff", "#ff0000" },
+	{ "#ffffff", "#ff7f00" },
+	{ "#000000", "#ffff00" },
+	{ "#000000", "#00ff00" },
+	{ "#ffffff", "#0000ff" },
+	{ "#ffffff", "#4b0082" },
+	{ "#ffffff", "#9400d3" },
+	{ "#000000", "#ffffff" },
+	{ "#ffffff", "#000000" },
+};
+
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
